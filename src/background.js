eef6684e026061b7afede752c45c707382e41dcf
Nazar Dub <dubnazar@gmail.com>
2021-11-18 03:05:20 +0200

    Remove YouTube Music support
     - Chrome creates sub-menu in context menu if
       multiple items used. I'm removing 'YouTube Music'
       for now, since it looks less usable for me

--- src/background.js
+++ src/background.js
@@ -1,5 +1,4 @@
 const YOUTUBE_CONTEXT_MENU_ITEM_ID = 'youtube-search-context-item'
-const YOUTUBE_MUSIC_CONTEXT_MENU_ITEM_ID = 'youtube-music-search-context-item'
 
 chrome.contextMenus.removeAll();
 
@@ -9,22 +8,13 @@ chrome.contextMenus.create({
         contexts: ['selection']
     }
 )
-chrome.contextMenus.create({
-        title: 'Search YouTube Music for: "%s"',
-        id: YOUTUBE_MUSIC_CONTEXT_MENU_ITEM_ID,
-        contexts: ['selection']
-    }
-)
 
 chrome.contextMenus.onClicked.addListener(
     function (info, tab) {
-        search = encodeURI(info.selectionText)
+        const search = encodeURI(info.selectionText)
         switch (info.menuItemId) {
             case YOUTUBE_CONTEXT_MENU_ITEM_ID:
                 chrome.tabs.create({url: 'https://www.youtube.com/results?search_query=' + search, active: true})
-                break
-            case YOUTUBE_MUSIC_CONTEXT_MENU_ITEM_ID:
-                chrome.tabs.create({url: 'https://music.youtube.com/search?q=' + search, active: true})
                 break
             default:
                 console.error('Unknown option clicked: ' + info)
